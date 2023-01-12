f8ec970f96d2e28045c0a5363d843ce9e55f121a
admin <dextercch.9033@gmail.com>
2023-01-12 19:34:00 +0800

    Initial Commit

---
+++ voiceguideApp/voiceguideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceguideAppApp.swift
+//  voiceguideApp
+//
+//  Created by Tim on 12/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct voiceguideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
